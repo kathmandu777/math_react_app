df40123b225f3f07a443ef0755261501cb2b55d0
mahhakatomanato@gmail.com <mahhakatomanato@gmail.com>
2020-04-15 12:47:12 +0900

    Updates

---
+++ precache-manifest.3de1a5fc7453ebaf3d214e59ccfb1eca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50b964161ea86fa0093ac444091b22de",
+    "url": "/kathmandu777/math_react_app.git/index.html"
+  },
+  {
+    "revision": "ed1db4cf3982fde09183",
+    "url": "/kathmandu777/math_react_app.git/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "aa68af17d711ab9c4324",
+    "url": "/kathmandu777/math_react_app.git/static/js/2.9b1c52d1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/kathmandu777/math_react_app.git/static/js/2.9b1c52d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed1db4cf3982fde09183",
+    "url": "/kathmandu777/math_react_app.git/static/js/main.601b237a.chunk.js"
+  },
+  {
+    "revision": "49318474f6f93b9bd6dc",
+    "url": "/kathmandu777/math_react_app.git/static/js/runtime-main.804e671f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/kathmandu777/math_react_app.git/static/media/logo.5d5d9eef.svg"
+  }
+]);
